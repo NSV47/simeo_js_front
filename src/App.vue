3c7752093b9264f54595b159b15f6269b8f4a27b
Sergey Novikov <nsv47nsv36@yandex.ru>
2020-03-09 20:00:56 +0300

    работает без фреймворка

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div>
-    <h1>Мое приложение</h1>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Карта</router-link>
-        </li>
-        <li>
-          <router-link to="/about">О проекте</router-link>
-        </li>
-      </ul>
-    </nav>
-    <!-- <Map v-if="page === 'Map'"></Map>
-    <About v-if="page === 'About'"></About> -->
-    <router-view></router-view>
-  </div>
-  
+  <v-app>
+    <div>
+      <Layout></Layout>
+    </div>
+  </v-app>
 </template>
 
 <script>
 // import Map from "./views/Map";
 // import About from "./views/About";
-
+import Layout from '@/views/Layout'
 export default {
   name: "App",
   components: {
     // Map,
     // About
+    Layout
   },
 
   data: () => ({
---
+++ src/views/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <h1>Мое приложение</h1>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/">Карта</router-link>
+        </li>
+        <li>
+          <router-link to="/about">О проекте</router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Layout",
+};
+</script>
